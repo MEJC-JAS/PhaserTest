9fb75d22e35a60f305c442489d57a883def098b2
Erwan Guillou <erwan.guillou@univ-lyon1.fr>
2018-09-25 11:30:34 +0200

    added player

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var IDE_HOOK = false;
 var VERSION = '2.6.2';
 
 var platforms;
+var player;
 
 function preload() {
     game.load.spritesheet('phaser', 'assets/dude.png');
@@ -24,27 +25,24 @@ function create() {
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
     platforms.enableBody = true;
-
-    // Here we create the ground.
     var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
-
-    //  Now let's create two ledges
     var ledge = platforms.create(400, 400, 'ground');
-
     ledge.body.immovable = true;
-
     ledge = platforms.create(-150, 250, 'ground');
-
     ledge.body.immovable = true;
+
+    // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
 }
 
 function update() {
