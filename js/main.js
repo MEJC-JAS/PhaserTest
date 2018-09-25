b15f6f57d6479516f2092ad9cca82abc26d4e1b9
Erwan Guillou <erwan.guillou@univ-lyon1.fr>
2018-09-25 11:03:59 +0200

    really added

--- js/main.js
+++ js/main.js
@@ -15,8 +15,39 @@ function preload() {
 }
 
 function create() {
-    var sprite = game.add.sprite(0, 0, 'phaser');
-    var carrot = game.add.sprite(400, 300, 'carrot');
+    //var sprite = game.add.sprite(0, 0, 'phaser');
+    //var carrot = game.add.sprite(400, 300, 'carrot');
+
+
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky');
+
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
+
+    //  We will enable physics for any object that is created in this group
+    platforms.enableBody = true;
+
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 2);
+
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+    //  Now let's create two ledges
+    var ledge = platforms.create(400, 400, 'ground');
+
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-150, 250, 'ground');
+
+    ledge.body.immovable = true;
 }
 
 function update() {
