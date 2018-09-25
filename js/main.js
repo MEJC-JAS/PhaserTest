3b1af27fc9fbbfe668c6af667c390ada1516f081
Erwan Guillou <erwan.guillou@univ-lyon1.fr>
2018-09-25 11:33:34 +0200

    effective spritesheet

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var platforms;
 var player;
 
 function preload() {
-    game.load.spritesheet('dude', 'assets/dude.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
